3ceb002e957ba764b568df953e97d873f77bc577
clarkgwapo <christianjames.ortouste>
2026-05-05 18:55:07 +0800

    sdfasdf

--- check_session.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/config/database.php';
-echo "<pre>";
-print_r($_SESSION);
-echo "</pre>";
-?>
--- generate_hash.php
+++
@@ -1,1 +0,0 @@
-<?php echo password_hash('genhash.php', PASSWORD_BCRYPT); ?>
